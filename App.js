4a3d4b62dc94ba59ef4977fd79610f1b38df6704
Shreya Singh <shreyaa700766@gmail.com>
2026-01-17 21:38:01 +0530

    basics

---
+++ App.js
@@ -0,0 +1,43 @@
+// const heading = React.createElement("h1", {}, "Hello world form React");
+
+//         const root=ReactDOM.createRoot(document.getElementById("root"));
+
+//         root.render(heading);
+
+
+/*
+<div id="parent">
+     <div id="child1">
+         <h1>i am h1 tag</h1>
+         <h2>i am h2 tag</h2>
+         </div>
+     <div id="child2">
+         <h1>i am h1 tag</h1>
+         <h2>i am h2 tag</h2>
+         </div>    
+</div>         
+
+*/ 
+
+const parent=React.createElement(
+        "div",
+        {id:"parent"},[
+                React.createElement("div", {id:"child1"},[
+                React.createElement("h1",{},"i am h1 tag"),
+                React.createElement("h2",{},"i am h2 tag"),
+        ]),
+        React.createElement("div", {id:"child2"},[
+                React.createElement("h1",{},"i am h1 tag"),
+                React.createElement("h2",{},"i am h2 tag"),
+        ])
+        ]
+        
+
+);
+
+
+console.log(parent) //object
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
